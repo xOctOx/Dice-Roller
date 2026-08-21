b1357604e6839e09fc9a5ce1ab99cb856c47cc35
Cheick Ouattara <cheickiouattata@gmail.com>
2026-08-21 08:51:26 +0000

    Ajout d'espace entre l'image et le bouton

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
@@ -19,6 +21,7 @@ import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,6 +56,9 @@ fun DiceWithButtonAndImages(modifier: Modifier = Modifier) {
             painter = painterResource(R.drawable.dice_1),
             contentDescription = "1"
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Button(onClick = {}) {
             Text(text = stringResource(R.string.roll))
         }
